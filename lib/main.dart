32d7c48bffa33bdffe9ad4ee70d7c9eb3facad01
Ayush10 <ayushgreat10@gmail.com>
2020-10-25 19:32:25 +0545

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Container(
+          height: 100,
+          width: 100,
+          color: Colors.white,
+          margin: EdgeInsets.all(40),
+          padding: EdgeInsets.all(20),
+          child: Text("Just Another text"),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
